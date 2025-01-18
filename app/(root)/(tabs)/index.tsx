fc2627beeef0ac0bc87b1c733eba2beb663455ff
Shaji-Mohammed <mshajisufiyan@gmail.com>
2025-01-18 19:26:39 -0400

    Redo Auth

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
         <Link href={"/sign-in"}> Sign In</Link>
         <Link href={"/explore"}> Explore</Link>
         <Link href={"/profile"}> Profile</Link>
-        <Link href={"/properties/1"}> Sign In</Link>
+        <Link href={"/properties/1"}> Properties</Link>
     </View>
   );
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,28 @@
-import {View, Text, ScrollView, Image, TouchableOpacity} from 'react-native'
+import {View, Text, ScrollView, Image, TouchableOpacity, Alert} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import {login} from "@/lib/appwrite";
+import {useGlobalContext} from "@/lib/global-provider";
+import {Redirect} from "expo-router";
 
 const SignIn = () => {
-    const handleLogin =  () => {}
+    const {refetch, loading, isLogged} = useGlobalContext();
+
+    if (!loading && isLogged) {
+        return <Redirect href={'/'} />;
+    }
+
+    const handleLogin =  async () => {
+        const result = await login();
+
+        if (result) {
+            refetch();
+        } else {
+            Alert.alert('Error', "Login Failed");
+        }
+    }
 
     return (
         <SafeAreaView className={"bg-white h-full"}>
@@ -22,7 +39,6 @@ const SignIn = () => {
                         Login to ReState with Google
                     </Text>
 
-                    // Button
                     <TouchableOpacity onPress={handleLogin} className={"bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"}>
                         <View className={"flex flex-row items-center justify-center"}>
                             <Image source={icons.google} className={"w-5 h-5"} resizeMode="contain" />
