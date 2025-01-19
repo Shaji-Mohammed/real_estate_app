39a9609942d468af642868cd793fe61734773d55
Shaji-Mohammed <mshajisufiyan@gmail.com>
2025-01-18 20:01:46 -0400

    bug fix

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -6,7 +6,7 @@ import {useGlobalContext} from "@/lib/global-provider";
 export default function AppLayout() {
     const {loading, isLoggedIn} = useGlobalContext();
 
-    if(true) {
+    if(loading) {
         return (
             <SafeAreaView className={"bg-white h-full flex justify-center items-center"}>
                 <ActivityIndicator className={"text-primary-300"} size="large"/>
