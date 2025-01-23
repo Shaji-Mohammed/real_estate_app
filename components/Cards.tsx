10dba09c17c1508065d5112f419af49ef6f19fec
Shaji <shaji@TC85B.WPA.Dal.Ca>
2025-01-23 14:49:52 -0400

    Cards component

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -34,10 +34,30 @@ export const FeaturedCard = ({onPress} : Props) => {
         </TouchableOpacity>
     )
 }
-export const Card = () => {
+export const Card = ({onPress} : Props) => {
     return (
-        <View>
-            <Text>Card</Text>
-        </View>
+        <TouchableOpacity className={"flex-1 w-full mt-4 px-3 py-4 bg-white shadow-lg rounded-lg shadow-black-100/70 relative"} onPress={onPress}>
+            <View className={"flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50"}>
+                <Image source={icons.star} className={"size-2.5"}/>
+                <Text className={"text-xs font-rubik-bold text-primary-300 ml-0.5 "}>4.3</Text>
+            </View>
+
+            <Image source={images.newYork} className={"h-40 w-full rounded-lg"} />
+
+            <View className={"flex flex-col mt-2"}>
+                <Text className={"text-base font-rubik-Bold text-black-300"} >
+                    Cozy Studio
+                </Text>
+                <Text className={"text-xs font-rubik text-black-200"}>
+                    2225 Monastery Lane, Halifax
+                </Text>
+                <View className={"flex flex-row items-center justify-between mt-2"}>
+                    <Text className={"text-base font-rubik-bold text-primary-300"}>
+                        $2500
+                    </Text>
+                    <Image source={icons.heart} className={"w-5 h-5 mr-2"} tintColor={"#191d31"} />
+                </View>
+            </View>
+        </TouchableOpacity>
     )
 }
